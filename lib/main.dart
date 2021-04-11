0de3b93f3b7dd3297c926d2f4792b7716c76354f
Anik7303 <anik7303@gmail.com>
2021-04-11 19:06:43 +0600

    using column

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,49 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            // mainAxisSize: MainAxisSize.min,
+            // verticalDirection: VerticalDirection.up,
+            // mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                color: Colors.white,
+                child: Text('Container 1'),
+                height: 100.0,
+                // margin: EdgeInsets.fromLTRB(20.0, 15.0, 30.0, 35.0),
+                // padding: EdgeInsets.all(15.0),
+                // width: 100.0,
+              ),
+              SizedBox(
+                height: 10.0,
+                width: 20.0,
+              ),
+              Container(
+                child: Text('Container 2'),
+                color: Colors.red,
+                height: 100.0,
+                // width: 100.0,
+              ),
+              SizedBox(
+                height: 10.0,
+                width: 20.0,
+              ),
+
+              Container(
+                child: Text('Container 3'),
+                color: Colors.amber,
+                height: 100.0,
+                // width: 100.0,
+              ),
+              // Container(
+              //   width: double.infinity,
+              //   color: Colors.black,
+              // )
+            ],
+          ),
+        ),
       ),
     );
   }
