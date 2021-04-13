7cd75efdb208f0f5e72702e09b1053726662a7fb
Anik7303 <anik7303@gmail.com>
2021-04-13 20:48:14 +0600

    using Card & Padding class instead of Container and using ListTile instead of Row

--- lib/main.dart
+++ lib/main.dart
@@ -16,80 +16,76 @@ class MyApp extends StatelessWidget {
             children: [
               CircleAvatar(
                 backgroundImage: AssetImage('images/anik.jpg'),
-                radius: 40.0,
+                radius: 50.0,
               ),
               Text(
                 'Anik Mohammad',
                 style: TextStyle(
                   color: Colors.white,
                   fontFamily: 'Pacifico',
-                  fontSize: 24.0,
+                  fontSize: 40.0,
                   fontWeight: FontWeight.bold,
                 ),
               ),
               Text(
-                'Software Developer',
+                'SOFTWARE DEVELOPER',
                 style: TextStyle(
                   color: Colors.teal.shade100,
                   fontFamily: 'SourceSansPro',
-                  fontSize: 18.0,
+                  fontSize: 20.0,
                   fontWeight: FontWeight.bold,
                   letterSpacing: 2.5,
                 ),
               ),
-              Container(
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.phone,
-                      color: Colors.teal.shade900,
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text('+8801943029170',
-                        style: TextStyle(
-                          color: Colors.teal.shade900,
-                          fontFamily: 'SourceSansPro',
-                          fontSize: 18.0,
-                        ))
-                  ],
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
                 ),
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 25.0,
-                ),
-                padding: EdgeInsets.all(6.0),
               ),
-              Container(
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.email,
+              Card(
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal.shade900,
+                  ),
+                  title: Text(
+                    '+8801943029170',
+                    style: TextStyle(
                       color: Colors.teal.shade900,
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20.0,
                     ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
-                      'anik7303@gmail.com',
-                      style: TextStyle(
-                        color: Colors.teal.shade900,
-                        fontFamily: 'SourceSansPro',
-                        fontSize: 18.0,
-                      ),
-                    )
-                  ],
+                  ),
                 ),
-                color: Colors.white,
                 margin: EdgeInsets.symmetric(
                   vertical: 10.0,
                   horizontal: 25.0,
                 ),
-                padding: EdgeInsets.all(6.0),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal.shade900,
+                  ),
+                  title: Text(
+                    'anik7303@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ),
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
               )
             ],
+            mainAxisAlignment: MainAxisAlignment.center,
           ),
         ),
       ),
