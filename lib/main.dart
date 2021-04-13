a254971ac2be018bab5f1711a101560facefdfcd
Anik7303 <anik7303@gmail.com>
2021-04-13 19:58:28 +0600

    Mi Card structure completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 void main() {
-  runApp(MiCard());
+  runApp(MyApp());
 }
 
-class MiCard extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Column(
-          children: [
-            CircleAvatar(
-              backgroundImage: AssetImage('images/anik.jpg'),
-              radius: 50.0,
-            )
-          ],
+        body: SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage('images/anik.jpg'),
+                radius: 40.0,
+              ),
+              Text(
+                'Anik Mohammad',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Pacifico',
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Software Developer',
+                style: TextStyle(
+                  color: Colors.teal.shade100,
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              Container(
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal.shade900,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text('+8801943029170',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 18.0,
+                        ))
+                  ],
+                ),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                padding: EdgeInsets.all(6.0),
+              ),
+              Container(
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal.shade900,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'anik7303@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 18.0,
+                      ),
+                    )
+                  ],
+                ),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                padding: EdgeInsets.all(6.0),
+              )
+            ],
+          ),
         ),
       ),
     );
