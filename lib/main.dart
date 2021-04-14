eea09464d17a324703801046c30c36bce17845af
Anik7303 <anik7303@gmail.com>
2021-04-14 18:08:26 +0600

    introduction statefull widget & math module

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -20,7 +22,15 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -28,16 +38,14 @@ class DicePage extends StatelessWidget {
         children: [
           Expanded(
             child: TextButton(
-              child: Image.asset('images/dice1.png'),
-              onPressed: () {
-                print('left button pressed');
-              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+              onPressed: onDicePress,
             ),
             flex: 2,
           ),
           Expanded(
             child: TextButton(
-              child: Image.asset('images/dice2.png'),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
               onPressed: onDicePress,
             ),
             flex: 2,
@@ -48,6 +56,9 @@ class DicePage extends StatelessWidget {
   }
 
   void onDicePress() {
-    print('dice pressed');
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
   }
 }
