92c456b1a7b505c8a4779c86e5157cfa2c090080
Anik7303 <anik7303@gmail.com>
2021-04-16 02:44:18 +0600

    finishing project

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,118 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Xylophone'),
-        ),
-        body: Container(),
-        backgroundColor: Colors.blue,
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: XylophoneApp(),
       ),
-    ),
-  );
+    ));
+  }
+}
+
+class XylophoneApp extends StatefulWidget {
+  @override
+  _XylophoneAppState createState() => _XylophoneAppState();
+}
+
+class _XylophoneAppState extends State<XylophoneApp> {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: TextButton(
+            onPressed: () {
+              playSound(1);
+            },
+            style: ButtonStyle(
+              padding: MaterialStateProperty.all(EdgeInsets.zero),
+              backgroundColor: MaterialStateProperty.all(Colors.red),
+            ),
+          ),
+        ),
+        Expanded(
+          child: TextButton(
+            onPressed: () {
+              playSound(2);
+            },
+            style: ButtonStyle(
+              padding: MaterialStateProperty.all(EdgeInsets.zero),
+              backgroundColor: MaterialStateProperty.all(Colors.orange),
+            ),
+          ),
+        ),
+        Expanded(
+          child: TextButton(
+            onPressed: () {
+              playSound(3);
+            },
+            style: ButtonStyle(
+              padding: MaterialStateProperty.all(EdgeInsets.zero),
+              backgroundColor: MaterialStateProperty.all(Colors.yellow),
+            ),
+          ),
+        ),
+        Expanded(
+          child: TextButton(
+            onPressed: () {
+              playSound(4);
+            },
+            style: ButtonStyle(
+              padding: MaterialStateProperty.all(EdgeInsets.zero),
+              backgroundColor: MaterialStateProperty.all(Colors.green),
+            ),
+          ),
+        ),
+        Expanded(
+          child: TextButton(
+            onPressed: () {
+              playSound(5);
+            },
+            style: ButtonStyle(
+              padding: MaterialStateProperty.all(EdgeInsets.zero),
+              backgroundColor: MaterialStateProperty.all(Colors.teal),
+            ),
+          ),
+        ),
+        Expanded(
+          child: TextButton(
+            onPressed: () {
+              playSound(6);
+            },
+            style: ButtonStyle(
+              padding: MaterialStateProperty.all(EdgeInsets.zero),
+              backgroundColor: MaterialStateProperty.all(Colors.blue),
+            ),
+          ),
+        ),
+        Expanded(
+          child: TextButton(
+            onPressed: () {
+              playSound(7);
+            },
+            style: ButtonStyle(
+              padding: MaterialStateProperty.all(EdgeInsets.zero),
+              backgroundColor: MaterialStateProperty.all(Colors.purple),
+            ),
+          ),
+        ),
+      ],
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+    );
+  }
 }
