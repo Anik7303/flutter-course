fb521e7f07c1ee2a0a16bbbc91ab6d4de290e8f5
Anik7303 <anik7303@gmail.com>
2021-04-16 02:59:00 +0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Container(),
+      ),
+    );
+  }
+}
