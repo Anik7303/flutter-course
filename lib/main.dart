67a1727c431320f0c66965cbd2fb1c304c811f5b
Anik7303 <anik7303@gmail.com>
2021-04-18 06:02:50 +0600

    finished project - destini

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'story_brain.dart';
+
 void main() => runApp(DestiniApp());
 
 class DestiniApp extends StatelessWidget {
@@ -20,11 +22,77 @@ class StoryPage extends StatefulWidget {
 }
 
 class _StoryPageState extends State<StoryPage> {
+  StoryBrain _storyBrain = StoryBrain();
+
+  Widget getChoiceButton({
+    String choice,
+    Color backgroundColor,
+    Function onPress,
+  }) {
+    return Expanded(
+      child: TextButton(
+        child: Text(
+          choice,
+          style: TextStyle(
+            fontSize: 20.0,
+            color: Colors.white,
+          ),
+        ),
+        onPressed: onPress,
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(backgroundColor),
+        ),
+      ),
+      flex: 1,
+    );
+  }
+
+  void onAnswer(int choice) {
+    setState(() {
+      _storyBrain.nextStory(choice);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: SafeArea(
-        child: Container(),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: Center(
+                  child: Text(
+                    _storyBrain.getStory(),
+                    style: TextStyle(fontSize: 25.0),
+                  ),
+                ),
+                flex: 8,
+              ),
+              getChoiceButton(
+                choice: _storyBrain.getChoice1(),
+                backgroundColor: Colors.red,
+                onPress: () {
+                  onAnswer(1);
+                },
+              ),
+              SizedBox(height: _storyBrain.shouldShowChoice2() ? 16.0 : 0.0),
+              Visibility(
+                child: getChoiceButton(
+                  choice: _storyBrain.getChoice2(),
+                  backgroundColor: Colors.blue,
+                  onPress: () {
+                    onAnswer(2);
+                  },
+                ),
+                visible: _storyBrain.shouldShowChoice2(),
+              )
+            ],
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+          ),
+        ),
       ),
       constraints: BoxConstraints.expand(),
       decoration: BoxDecoration(
