cf339e17a995cf1fb7b00c7f5324bed377da4433
Anik7303 <anik7303@gmail.com>
2021-04-19 06:59:26 +0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'input_page.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  final ThemeData theme = ThemeData.dark().copyWith(
+    primaryColor: Color(0xFF0A0E21),
+    scaffoldBackgroundColor: Color(0xFF0A0E21),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: InputPage(),
+        ),
+      ),
+      title: 'BMI Calculator',
+      theme: this.theme,
+    );
+  }
+}
